7707fb8cbb8232288fd8c57616c275d099630c9a
Byron2016 <bgva2005@yahoo.com>
2024-07-15 13:39:32 -0500

    b 2

--- b.ts
+++ b.ts
@@ -1,4 +1,3 @@
-console.log("Hello via Bun!!!!")
-console.log("Hello via Bun1!!!!")
-console.log("Hello via Bun1!!!!")
-    
+console.log('Hello via Bun!!!!')
+console.log('Hello via Bun1!!!!')
+console.log('Hello via Bun1!!!!')
